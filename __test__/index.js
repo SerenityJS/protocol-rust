43c344562adde965ddcf7714cce4410b9f6cc7b8
Logan <logan@amihan.gg>
2023-07-11 08:32:14 -0500

    update packet to follow correct formatting

--- __test__/index.js
+++ __test__/index.js
@@ -1,13 +1,28 @@
 const { serializeResourcePacksInfo, deserializeResourcePacksInfo } = require('../index.js')
 
+let d = Date.now()
 const buffer = serializeResourcePacksInfo({
   hasScripts: false,
   mustAccept: true,
-  resourcePacks: ['hello_world'],
-  behaviourPacks: ['foo_bar_baz']
+  resourcePacks: [
+    {
+      'uuid': '59addd86-1fee-11ee-be56-0242ac120002',
+      'size': 10000n,
+      'version': '1.0.0',
+      'subPackName': 'foo bar baz',
+      'contentKey': 'foo_bar_baz',
+      'contentIdentity': 'foo_bar_baz',
+      'hasScripts': false,
+      'rtxEnabled': false,
+    }  
+  ],
+  behaviourPacks: []
 })
 const buf = Buffer.from(buffer)
+console.log('Took ' + (Date.now() - d) + 'ms to serialize')
 console.log(buf)
 
-const deserialized = deserializeResourcePacksInfo(Array.from(buf))
-console.log(deserialized)
+d = Date.now()
+const des = deserializeResourcePacksInfo(Array.from(buf))
+console.log('Took ' + (Date.now() - d) + 'ms to deserialize')
+console.log(des)
--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,93 @@
 #![deny(clippy::all)]
 
+use napi::bindgen_prelude::*;
+
 #[macro_use]
 extern crate napi_derive;
 
 #[napi(object)]
-pub struct ResourcePacksInfo {
-  pub must_accept: bool,
+pub struct BehaviourPackInfo {
+  pub uuid: String,
+  pub version: String,
+  pub size: BigInt,
+  pub content_key: String,
+  pub sub_pack_name: String,
+  pub content_identity: String,
   pub has_scripts: bool,
-  pub behaviour_packs: Vec<String>,
-  pub resource_packs: Vec<String>,
 }
 
-// 0x07
+#[napi(object)]
+pub struct ResourcePackInfo {
+  pub uuid: String,
+  pub version: String,
+  pub size: BigInt,
+  pub content_key: String,
+  pub sub_pack_name: String,
+  pub content_identity: String,
+  pub has_scripts: bool,
+  pub rtx_enabled: bool,
+}
+
+#[napi(object)]
+pub struct ResourcePacksInfoPacket {
+  pub must_accept: bool,
+  pub has_scripts: bool,
+  pub behaviour_packs: Vec<BehaviourPackInfo>,
+  pub resource_packs: Vec<ResourcePackInfo>,
+}
 
 #[napi]
+impl ResourcePacksInfoPacket {
+  fn packet_id() -> u8 {
+    0x07
+  }
+}
+
+
 // Generates raknet packet buffer for resource packs info minecraft bedrock edition packet
-pub fn serialize_resource_packs_info(info: ResourcePacksInfo) -> Vec<u8> {
+#[napi]
+pub fn serialize_resource_packs_info(info: ResourcePacksInfoPacket) -> Vec<u8> {
   let mut buffer = Vec::new();
-  buffer.push(0x07);
+  buffer.push(ResourcePacksInfoPacket::packet_id());
   buffer.push(if info.must_accept { 1 } else { 0 });
   buffer.push(if info.has_scripts { 1 } else { 0 });
   buffer.push(info.behaviour_packs.len() as u8);
   for pack in info.behaviour_packs {
-    buffer.push(pack.len() as u8);
-    buffer.extend(pack.as_bytes());
+    buffer.push(pack.uuid.len() as u8);
+    buffer.extend(pack.uuid.as_bytes());
+    buffer.push(pack.version.len() as u8);
+    buffer.extend(pack.version.as_bytes());
+    buffer.extend(pack.size.get_u64().1.to_be_bytes().iter());
+    buffer.push(pack.content_key.len() as u8);
+    buffer.extend(pack.content_key.as_bytes());
+    buffer.push(pack.sub_pack_name.len() as u8);
+    buffer.extend(pack.sub_pack_name.as_bytes());
+    buffer.push(pack.content_identity.len() as u8);
+    buffer.extend(pack.content_identity.as_bytes());
+    buffer.push(if pack.has_scripts { 1 } else { 0 });
   }
   buffer.push(info.resource_packs.len() as u8);
   for pack in info.resource_packs {
-    buffer.push(pack.len() as u8);
-    buffer.extend(pack.as_bytes());
+    buffer.push(pack.uuid.len() as u8);
+    buffer.extend(pack.uuid.as_bytes());
+    buffer.push(pack.version.len() as u8);
+    buffer.extend(pack.version.as_bytes());
+    buffer.extend(pack.size.get_u64().1.to_be_bytes().iter());
+    buffer.push(pack.content_key.len() as u8);
+    buffer.extend(pack.content_key.as_bytes());
+    buffer.push(pack.sub_pack_name.len() as u8);
+    buffer.extend(pack.sub_pack_name.as_bytes());
+    buffer.push(pack.content_identity.len() as u8);
+    buffer.extend(pack.content_identity.as_bytes());
+    buffer.push(if pack.has_scripts { 1 } else { 0 });
+    buffer.push(if pack.rtx_enabled { 1 } else { 0 });
   }
   buffer
 }
 
-
+// Deserializes raknet packet buffer for resource packs info minecraft bedrock edition packet
 #[napi]
-pub fn deserialize_resource_packs_info(data: Vec<u8>) -> ResourcePacksInfo {
+pub fn deserialize_resource_packs_info(data: Vec<u8>) -> ResourcePacksInfoPacket {
   let mut data = data.into_iter();
   let _id = data.next().unwrap();
   let must_accept = data.next().unwrap() == 1;
@@ -43,24 +95,105 @@ pub fn deserialize_resource_packs_info(data: Vec<u8>) -> ResourcePacksInfo {
   let behaviour_packs_len = data.next().unwrap();
   let mut behaviour_packs = Vec::new();
   for _ in 0..behaviour_packs_len {
-    let pack_len = data.next().unwrap();
-    let mut pack = Vec::new();
-    for _ in 0..pack_len {
-      pack.push(data.next().unwrap());
+    let uuid_len = data.next().unwrap();
+    let mut uuid = Vec::new();
+    for _ in 0..uuid_len {
+      uuid.push(data.next().unwrap());
     }
-    behaviour_packs.push(String::from_utf8(pack).unwrap());
+    let uuid = String::from_utf8(uuid).unwrap();
+    let version_len = data.next().unwrap();
+    let mut version = Vec::new();
+    for _ in 0..version_len {
+      version.push(data.next().unwrap());
+    }
+    let version = String::from_utf8(version).unwrap();
+    let mut size = [0; 8];
+    for i in 0..8 {
+      size[i] = data.next().unwrap();
+    }
+    let size = BigInt::from(u64::from_be_bytes(size));
+    let content_key_len = data.next().unwrap();
+    let mut content_key = Vec::new();
+    for _ in 0..content_key_len {
+      content_key.push(data.next().unwrap());
+    }
+    let content_key = String::from_utf8(content_key).unwrap();
+    let sub_pack_name_len = data.next().unwrap();
+    let mut sub_pack_name = Vec::new();
+    for _ in 0..sub_pack_name_len {
+      sub_pack_name.push(data.next().unwrap());
+    }
+    let sub_pack_name = String::from_utf8(sub_pack_name).unwrap();
+    let content_identity_len = data.next().unwrap();
+    let mut content_identity = Vec::new();
+    for _ in 0..content_identity_len {
+      content_identity.push(data.next().unwrap());
+    }
+    let content_identity = String::from_utf8(content_identity).unwrap();
+    let has_scripts = data.next().unwrap() == 1;
+    behaviour_packs.push(BehaviourPackInfo {
+      uuid,
+      version,
+      size,
+      content_key,
+      sub_pack_name,
+      content_identity,
+      has_scripts,
+    });
   }
   let resource_packs_len = data.next().unwrap();
   let mut resource_packs = Vec::new();
   for _ in 0..resource_packs_len {
-    let pack_len = data.next().unwrap();
-    let mut pack = Vec::new();
-    for _ in 0..pack_len {
-      pack.push(data.next().unwrap());
+    let uuid_len = data.next().unwrap();
+    let mut uuid = Vec::new();
+    for _ in 0..uuid_len {
+      uuid.push(data.next().unwrap());
     }
-    resource_packs.push(String::from_utf8(pack).unwrap());
+    let uuid = String::from_utf8(uuid).unwrap();
+    let version_len = data.next().unwrap();
+    let mut version = Vec::new();
+    for _ in 0..version_len {
+      version.push(data.next().unwrap());
+    }
+    let version = String::from_utf8(version).unwrap();
+    let mut size = [0; 8];
+    for i in 0..8 {
+      size[i] = data.next().unwrap();
+    }
+    let size = BigInt::from(u64::from_be_bytes(size));
+    let content_key_len = data.next().unwrap();
+    let mut content_key = Vec::new();
+    for _ in 0..content_key_len {
+      content_key.push(data.next().unwrap());
+    }
+    let content_key = String::from_utf8(content_key).unwrap();
+    let sub_pack_name_len = data.next().unwrap();
+    let mut sub_pack_name = Vec::new();
+    for _ in 0..sub_pack_name_len {
+      sub_pack_name.push(data.next().unwrap());
+    }
+    let sub_pack_name = String::from_utf8(sub_pack_name).unwrap();
+    let content_identity_len = data.next().unwrap();
+    let mut content_identity = Vec::new();
+    for _ in 0..content_identity_len {
+      content_identity.push(data.next().unwrap());
+    }
+    let content_identity = String::from_utf8(content_identity).unwrap();
+    let has_scripts = data.next().unwrap() == 1;
+    let rtx_enabled = data.next().unwrap() == 1;
+    resource_packs.push(ResourcePackInfo {
+      uuid,
+      version,
+      size,
+      content_key,
+      sub_pack_name,
+      content_identity,
+      has_scripts,
+      rtx_enabled,
+    });
   }
-  ResourcePacksInfo {
+
+  ResourcePacksInfoPacket {
     must_accept,
     has_scripts,
     behaviour_packs,
