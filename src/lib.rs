86609cc06dfc5adda8deac9bb01451f6eb891900
Logan <logan@amihan.gg>
2023-07-11 20:49:42 -0500

    login packet works!!

--- src/lib.rs
+++ src/lib.rs
@@ -16,44 +16,6 @@ use napi::bindgen_prelude::*;
 //   pub foo: i32,
 // }
 
-#[napi(object)]
-pub struct BehaviourPackInfo {
-  pub uuid: String,
-  pub version: String,
-  pub size: BigInt,
-  pub content_key: String,
-  pub sub_pack_name: String,
-  pub content_identity: String,
-  pub has_scripts: bool,
-}
-
-#[napi(object)]
-pub struct ResourcePackInfo {
-  pub uuid: String,
-  pub version: String,
-  pub size: BigInt,
-  pub content_key: String,
-  pub sub_pack_name: String,
-  pub content_identity: String,
-  pub has_scripts: bool,
-  pub rtx_enabled: bool,
-}
-
-#[napi(object)]
-pub struct ResourcePacksInfoPacket {
-  pub must_accept: bool,
-  pub has_scripts: bool,
-  pub behaviour_packs: Vec<BehaviourPackInfo>,
-  pub resource_packs: Vec<ResourcePackInfo>,
-}
-
-#[napi]
-impl ResourcePacksInfoPacket {
-  fn packet_id() -> u8 {
-    0x07
-  }
-}
-
 fn write_bool(buffer: &mut Vec<u8>, value: bool) {
   buffer.push(if value { 1 } else { 0 });
 }
@@ -96,6 +58,101 @@ fn read_u8(data: &mut impl Iterator<Item = u8>) -> u8 {
   data.next().unwrap()
 }
 
+fn read_i32(data: &mut impl Iterator<Item = u8>) -> i32 {
+  let mut bytes = [0; 4];
+  for i in 0..4 {
+    bytes[i] = data.next().unwrap();
+  }
+  i32::from_be_bytes(bytes)
+}
+
+// read little endian u32
+fn read_u32_le(data: &mut impl Iterator<Item = u8>) -> u32 {
+  let mut bytes = [0; 4];
+  for i in 0..4 {
+    bytes[i] = data.next().unwrap();
+  }
+  u32::from_le_bytes(bytes)
+}
+
+// read minecraft bedrock unsigned varint (little endian) (max 5 bytes)
+fn read_varint(data: &mut impl Iterator<Item = u8>) -> u32 {
+  let mut num_read = 0;
+  let mut result = 0;
+  loop {
+    let read = data.next().unwrap();
+    let value = (read & 0b01111111) as u32;
+    result |= value << (7 * num_read);
+    num_read += 1;
+    if num_read > 5 {
+      panic!("VarInt is too big");
+    }
+    if (read & 0b10000000) == 0 {
+      break;
+    }
+  }
+  result
+}
+
+// read minecraft byte array
+fn read_byte_array(data: &mut impl Iterator<Item = u8>) -> Vec<u8> {
+  let len = read_varint(data);
+  let mut array = Vec::new();
+  for _ in 0..len {
+    array.push(data.next().unwrap());
+  }
+  array
+}
+
+// read minecraft bedrock little string
+fn read_little_string(data: &mut impl Iterator<Item = u8>) -> String {
+  let len = read_u32_le(data);
+  println!("len: {}", len);
+  let mut string = String::new();
+  for _ in 0..len {
+    string.push(data.next().unwrap() as char);
+  }
+  string
+}
+
+
+#[napi(object)]
+pub struct BehaviourPackInfo {
+  pub uuid: String,
+  pub version: String,
+  pub size: BigInt,
+  pub content_key: String,
+  pub sub_pack_name: String,
+  pub content_identity: String,
+  pub has_scripts: bool,
+}
+
+#[napi(object)]
+pub struct ResourcePackInfo {
+  pub uuid: String,
+  pub version: String,
+  pub size: BigInt,
+  pub content_key: String,
+  pub sub_pack_name: String,
+  pub content_identity: String,
+  pub has_scripts: bool,
+  pub rtx_enabled: bool,
+}
+
+#[napi(object)]
+pub struct ResourcePacksInfoPacket {
+  pub must_accept: bool,
+  pub has_scripts: bool,
+  pub behaviour_packs: Vec<BehaviourPackInfo>,
+  pub resource_packs: Vec<ResourcePackInfo>,
+}
+
+#[napi]
+impl ResourcePacksInfoPacket {
+  fn packet_id() -> u8 {
+    0x07
+  }
+}
 
 // Generates raknet packet buffer for resource packs info minecraft bedrock edition packet
 #[napi]
@@ -190,5 +247,46 @@ pub fn deserialize_resource_packs_info(data: Vec<u8>) -> ResourcePacksInfoPacket
     has_scripts,
     behaviour_packs,
     resource_packs,
+  }
+}
+
+#[napi(object)]
+pub struct LoginToken {
+  pub identity: String,
+  pub client: String,
+}
+
+#[napi(object)]
+pub struct LoginPacket {
+  pub protocol_version: i32,
+  pub tokens: LoginToken,
+}
+
+#[napi]
+impl LoginPacket {
+  pub fn packet_id() -> u8 {
+    0x01
+  }
+}
+
+// Deserializes raknet packet buffer for login minecraft bedrock edition packet
+#[napi]
+pub fn deserialize_login(data: Vec<u8>) -> LoginPacket {
+  let mut data = data.into_iter();
+  let _id = read_u8(&mut data);
+  let protocol_version = read_i32(&mut data);
+  // let mut bytes = read_byte_array(&mut data).into_iter();
+  // let client = read_little_string(&mut bytes);
+  read_varint(&mut data);
+
+  let identity = read_little_string(&mut data);
+  let client = read_little_string(&mut data);
+
+  LoginPacket {
+    protocol_version,
+    tokens: LoginToken {
+      identity,
+      client,
+    },
   }
 }
