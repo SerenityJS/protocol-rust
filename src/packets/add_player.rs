04e6573776982fed16f9787b807e59445a1fe0ba
PMK744 <69542280+PMK744@users.noreply.github.com>
2023-08-14 11:39:42 -0500

    Updated AddPlayer packet

--- src/packets/add_player.rs
+++ src/packets/add_player.rs
@@ -1,5 +1,6 @@
 use protocol_derive::packet;
 use crate::binary::prelude::*;
+use napi::bindgen_prelude::*;
 use crate::packets::{
   prelude::*,
   types::*
@@ -16,16 +17,86 @@ pub struct AddPlayerPacket {
   pub pitch: LF32,
   pub yaw: LF32,
   pub head_yaw: LF32,
-  // TODO: pub item: VarInt,
+  pub item: Item,
   #[napi(ts_type = "GameMode")]
   pub gamemode: ZigZag,
-  // TODO: pub metadata: MetadataDictionary,
-  // TODO: pub properties: EntityProperties,
+  #[VarInt] pub metadata: Vec<MetadataDictionary>,
+  pub properties: EntityProperties,
   pub unique_id: LI16,
-  // TODO: pub permission_level: PermissionLevel
-  // TODO: pub command_permission_level: CommandPermissionLevel,
-  // TODO: pub abilities: Abilities,
-  // TODO: pub links: Links
+  #[napi(ts_type = "PermissionLevel")]
+  pub permission_level: u8,
+  #[napi(ts_type = "CommandPermissionLevel")]
+  pub command_permission_level: u8,
+  pub abilities: AbilityLayers,
+  #[VarInt] pub links: Vec<Link>,
   pub device_id: String,
-  // TODO: pub device_os: DeviceOS,
+  // TODO: Added device list...
+  pub device_os: LI32,
+}
+
+// TODO: handle network_id fields
+#[packet]
+pub struct Item {
+  pub network_id: ZigZag,
+}
+
+#[packet]
+pub struct MetadataDictionary {
+  pub key: VarInt,
+  pub key_type: VarInt,
+  // TODO: pub value
+}
+
+#[packet]
+pub struct EntityProperties {
+  #[VarInt] pub ints: Vec<EntityInts>,
+  #[VarInt] pub floats: Vec<EntityFloats>,
+}
+
+#[packet]
+pub struct EntityInts {
+  pub index: VarInt,
+  pub value: ZigZag,
+}
+
+#[packet]
+pub struct EntityFloats {
+  pub index: VarInt,
+  pub value: LF32,
+}
+
+#[napi]
+pub enum PermissionLevel {
+  Visitor,
+  Member,
+  Operator,
+  Custom,
+}
+
+#[napi]
+pub enum CommandPermissionLevel {
+  Normal,
+  Operator,
+  Automation,
+  Host,
+  Owner,
+  Internal,
+}
+
+#[packet]
+pub struct AbilityLayers {
+  pub ability_type: LU16,
+  // TODO: pub allowed: AbilitySet,
+  // TODO: pub enabled: AbilitySet,
+  pub fly_speed: LF32,
+  pub walk_speed: LF32,
+}
+
+#[packet]
+pub struct Link {
+  pub ridden_entity_id: ZigZong,
+  pub rider_entity_id: ZigZong,
+  pub link_type: u8,
+  pub immediate: bool,
+  pub rider_initiated: bool,
 }
